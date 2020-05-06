2808290fdd6c3ef8059cb85b04ceeb60318e9f48
aviadp <apolak@redhat.com>
2020-05-06 19:34:51 +0300

    file created
    Signed-off-by: aviadp <apolak@redhat.com>

---
+++ tests/ecosystem/upgrade/test_ocs_upgrade_shutdown.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.mark.polarion_id("OCS-1574")
+def test_perm_shutdown_master():
+    pass
+
+
+@pytest.mark.polarion_id("OCS-1575")
+def test_perm_shutdown_worker():
+    pass
+
+
+@pytest.mark.polarion_id("OCS-1578")
+def test_abrupt_shutdown_master():
+    pass
+
+
+@pytest.mark.polarion_id("OCS-1579")
+def test_abrupt_shutdown_worker():
+    pass
